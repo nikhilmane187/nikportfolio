0ef0196a3f1a36d9e997a07bc0880a2e4206bdbe
nikhil <nikhilmane187@gmail.com>
2022-10-17 21:09:36 +0530

    Updates

---
+++ precache-manifest.2d19f435ba174a8199c10d2ac962bdc9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "68adcec16f11943b4cb9",
+    "url": "/nikportfolio/static/css/main.9dec53ae.chunk.css"
+  },
+  {
+    "revision": "68adcec16f11943b4cb9",
+    "url": "/nikportfolio/static/js/main.68adcec1.chunk.js"
+  },
+  {
+    "revision": "c327fb9723080ee1e8a9",
+    "url": "/nikportfolio/static/js/1.c327fb97.chunk.js"
+  },
+  {
+    "revision": "6e2a3e0df0af81a8cca8",
+    "url": "/nikportfolio/static/js/runtime~main.6e2a3e0d.js"
+  },
+  {
+    "revision": "b620b6e24f97e20760810cf52896312e",
+    "url": "/nikportfolio/static/media/me-about3.b620b6e2.jpg"
+  },
+  {
+    "revision": "28cfec4ced01a164d361e009bd8d8ada",
+    "url": "/nikportfolio/static/media/Nikhil_Resume_JS.28cfec4c.pdf"
+  },
+  {
+    "revision": "14e26e7bf5c8a170cc89280ad0cb1837",
+    "url": "/nikportfolio/static/media/me.14e26e7b.jpg"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/nikportfolio/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/nikportfolio/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/nikportfolio/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/nikportfolio/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/nikportfolio/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/nikportfolio/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "cde4610875a8deaeb305f7370dfb1752",
+    "url": "/nikportfolio/index.html"
+  }
+];
