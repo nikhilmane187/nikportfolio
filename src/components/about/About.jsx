013260ef0fe62034bb0f0f35c38f45ff84d6f58e
Nikhil Mane <nikhilmane187@gmail.com>
2023-11-28 22:44:36 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -25,13 +25,13 @@ const About = () => {
             <article className='about__card'>
               <FaAward className='about__icon' />
               <h5>Experience</h5>
-              <small>1+ Year's Working</small>
+              <small>2+ Year's Working</small>
 
             </article>
             <article className='about__card'>
               <FiUsers className='about__icon' />
               <h5>Clients</h5>
-              <small>2+ Worldwide</small>
+              <small>1+ Worldwide</small>
 
             </article>
             <article className='about__card'>
@@ -43,7 +43,7 @@ const About = () => {
           </div>
 
           <p>
-            Innovative Software Developer with 1+ years’ experience building and maintaining responsive websites in the recruiting industry. Proficient in HTML, CSS, jQuery, SQL, JavaScript, React Js and Angular, plus modern libraries and frameworks. Passionate about usability and possess working knowledge of Jira, Bit bucket & GitHub.
+            Innovative software developer with 2+ years of experience building and maintaining databases and websites in the software industry. Proficient in HTML, CSS, jQuery, SQL, JavaScript, postgres SQL, PL SQL, React Js, and Angular, plus modern libraries and frameworks. Passionate about usability and possess a working knowledge of Jira, Bitbucket & GitHub.
           </p>
           <a href="#contact" className='btn btn-primary'>Let's Talk</a>
 
